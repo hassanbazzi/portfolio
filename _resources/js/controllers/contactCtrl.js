f8f414851bc4f81e014aee9d872645bdf188395e
Hassan Bazzi <info@websiteme.org>
2014-12-18 20:09:29 -0500

    Fix to Mail

--- _resources/js/controllers/contactCtrl.js
+++ _resources/js/controllers/contactCtrl.js
@@ -1,25 +1,15 @@
 app.controller('contactCtrl', ['$scope', '$http', function($scope,$http){
 	$scope.sendmessage = function(){
 		console.log('in here');
-		//if(!$scope.contactform.$invalid){
-			$http({
-    url: 'https://www.google.com/recaptcha/api/siteverify', 
-    method: "GET",
-    params: {secret: '6LecfP8SAAAAAI_jjo8CNAYa8AQSC1YRwnFibq85', response: $scope.gresponse, callback : '?'}
- })
-			.success(function(data){
-				$http.post('/mailer/mail.php', {name : $scope.name, email : $scope.email, message : $scope.message}).
-				  success(function(data, status, headers, config) {
-				    console.log(data);
-				  }).
-				  error(function(data, status, headers, config) {
-				    console.log(data);
-				  });
+		if(!$scope.contactform.$invalid){
+			$http.post('/mailer/mail.php', {name : $scope.name, email : $scope.email, message : $scope.message, g-recaptcha-response : $scope.gresponse})
+			.success(function(data, status, headers, config) {
+			    console.log(data);
 			})
-			.error(function(data){
-				console.log(data);
+			.error(function(data, status, headers, config) {
+			    console.log(data);
 			});
-		//}
+		}
 		return false;
 	}
 	setresponse = function(response){
